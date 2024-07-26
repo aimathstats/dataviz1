ee58d212065c943412164f45fdeadb43a12a5f60
aimathstats <aimathstats@gmail.com>
2024-07-26 17:21:28 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import streamlit as st
 
 st.set_page_config(layout="wide")
 
+# additional codes
 df2 = pd.read_csv('data/koukouseiseki.csv')
 df3 = pd.read_csv('data/nikkei225.csv')
 vars2 = [var for var in df2.columns]
@@ -27,16 +28,14 @@ fig_target.update_traces(textposition='inside', textinfo='label+percent')
 
 
 # Layout (Sidebar)
-#st.sidebar.markdown("## Settings")
 st.sidebar.markdown("## Selection in Sidebar")
 vars2_selected = st.sidebar.selectbox('高校科目', vars2)
-vars2_multi_selected = st.sidebar.multiselect('高校科目', vars2, default=vars2)
+vars2_multi_selected = st.sidebar.multiselect('高校科目', vars2, default=vars2) # デフォルトは全部
 vars3_selected = st.sidebar.selectbox('日経225系列', vars3)
 
 cat_selected = st.sidebar.selectbox('Categorical Variables', vars_cat)
 cont_selected = st.sidebar.selectbox('Continuous Variables', vars_cont)
-cont_multi_selected = st.sidebar.multiselect('Correlation Matrix', vars_cont,
-                                             default=vars_cont)
+cont_multi_selected = st.sidebar.multiselect('Correlation Matrix', vars_cont, default=vars_cont)
 
 st.sidebar.markdown("## Target Variables")
 st.sidebar.plotly_chart(fig_target, use_container_width=True)
@@ -52,7 +51,6 @@ fig_cat = go.Figure(data=[
     go.Bar(name='target=0', x=cat0[cat_selected], y=cat0['id']),
     go.Bar(name='target=1', x=cat1[cat_selected], y=cat1['id'])
 ])
-
 fig_cat.update_layout(height=300,
                       width=500,
                       margin={'l': 20, 'r': 20, 't': 0, 'b': 0},
@@ -64,6 +62,7 @@ fig_cat.update_layout(height=300,
                       barmode='stack')
 fig_cat.update_xaxes(title_text=None)
 fig_cat.update_yaxes(title_text='# of samples')
+
 
 # Continuous Variable Distribution in Content
 li_cont0 = df[df['target'] == 0][cont_selected].values.tolist()
@@ -84,6 +83,14 @@ fig_cont.update_layout(height=300,
                            xanchor="right",
                            x=0.99)
                        )
+# Correlation Matrix of kamoku in Content
+df2_corr = df[vars2_multi_selected].corr()
+fig_corr2 = go.Figure([go.Heatmap(z=df2_corr.values,
+                                  x=df2_corr.index.values,
+                                  y=df2_corr.columns.values)])
+fig_corr2.update_layout(height=300,
+                        width=1000,
+                        margin={'l': 20, 'r': 20, 't': 0, 'b': 0})
 
 # Correlation Matrix in Content
 df_corr = df[cont_multi_selected].corr()
@@ -94,11 +101,17 @@ fig_corr.update_layout(height=300,
                        width=1000,
                        margin={'l': 20, 'r': 20, 't': 0, 'b': 0})
 
+
 # Layout (Content)
 left_column, right_column = st.columns(2)
+
 left_column.subheader('Categorical Variable Distribution: ' + cat_selected)
 right_column.subheader('Continuous Variable Distribution: ' + cont_selected)
 left_column.plotly_chart(fig_cat)
 right_column.plotly_chart(fig_cont)
+
 st.subheader('Correlation Matrix')
 st.plotly_chart(fig_corr)
+
+st.subheader('Correlation Matrix of kamoku')
+st.plotly_chart(fig_corr2)
