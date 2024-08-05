8a043bda81a2e09f516a84865b9090ce754037c2
aimathstats <aimathstats@gmail.com>
2024-08-05 17:15:11 +0900

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,17 @@ import numpy as np
 
 st.set_page_config(layout="wide")
 
+# contour plot
+fig27 = go.Figure(data=go.Contour(
+            z=[[10, 10.625, 12.5, 15.625, 20],
+            [5.625, 6.25, 8.125, 11.25, 15.625],
+            [2.5, 3.125, 5., 8.125, 12.5],
+            [0.625, 1.25, 3.125, 6.25, 10.625],
+            [0, 0.625, 2.5, 5.625, 10]]
+            ))
+st.subheader('contour plot')
+st.plotly_chart(fig27)
+
 # ridgeline plot
 from plotly.colors import n_colors
 # 12 sets of normal distributed random data, with increasing mean and standard deviation
