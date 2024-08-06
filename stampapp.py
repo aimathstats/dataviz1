ac80fe505a193b78febe30ae82dc13af482f5ba6
aimathstats <aimathstats@gmail.com>
2024-08-06 21:15:24 +0900

    Update stampapp.py

--- stampapp.py
+++ stampapp.py
@@ -2,7 +2,8 @@ import streamlit as st
 import fitz
 import requests
 import pandas as pd
- 
+ import plotly.express as px
+
 # PDFファイルのURL
 #https://www.mhlw.go.jp/stf/seisakunitsuite/bunya/0000121431_00461.html
 url = 'https://www.mhlw.go.jp/content/001282915.pdf'
