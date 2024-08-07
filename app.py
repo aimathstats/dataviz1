0aa0eae17e4c43a90c065de8b7c7aba21ffd0c19
aimathstats <aimathstats@gmail.com>
2024-08-07 10:07:44 +0900

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ kw_list = ["Python"]
 pytrends.build_payload(kw_list, timeframe='2014-01-01 2018-09-30', geo='JP')
 df = pytrends.interest_over_time()
 #df.plot(figsize=(15, 3), lw=.7)
-st.write(df)
+#.write(df)
 
 # PDFからのテーブル取得と可視化：都道府県別コロナ定点観測の折れ線
 import fitz
