f8bb593a0d1def638df61717fa3035e0fe5a3ba3
aimathstats <aimathstats@gmail.com>
2024-08-08 10:30:25 +0900

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ kw_list = ["AI","データサイエンス","ChatGPT"]
 pytrends.build_payload(kw_list, timeframe='2013-01-01 2024-08-05', geo='JP')
 df = pytrends.interest_over_time()
 #df.drop(columns=['isPartial'], inplace=True)
-df.reset_index(inplace=True)
+#df.reset_index(inplace=True)
 st.dataframe(df)
 
 fig33 = px.line(df, x='date', y=kw_list)
