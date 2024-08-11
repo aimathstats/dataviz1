042189fbfb7a9315c2c6b9fdafaf0f4b1efa8d4c
aimathstats <aimathstats@gmail.com>
2024-08-11 16:31:04 +0900

    Update app.py

--- app.py
+++ app.py
@@ -849,6 +849,23 @@ date_str = yesterday.strftime('%Y-%m-%d')
 pytrends = TrendReq(hl='ja-JP', tz=360)
 kw_list = ["AI","ChatGPT"]
 #kw_list = ["データサイエンス"]
+kw_list = [
+    "Pretender",
+    "宿命",
+    "I LOVE...",
+    "イエスタデイ",
+    "ノーダウト",
+    "Laughter",
+    "Cry Baby",
+    "115万キロのフィルム",
+    "FIRE GROUND",
+    "ブラザーズ",
+    "異端なスター",
+    "バッドフォーミー",
+    "Universe",
+    "パラボラ",
+    "アポトーシス"
+]
 pytrends.build_payload(kw_list, timeframe='2020-01-01 2024-08-05', geo='JP')
 df = pytrends.interest_over_time().drop(columns=['isPartial'])
 df.reset_index(inplace=True)
