06d54b8c9ccf69a86d35f54cb003606107ef2194
aimathstats <aimathstats@gmail.com>
2024-08-20 21:36:24 +0900

    Update app_preliminary.py

--- app_preliminary.py
+++ app_preliminary.py
@@ -15,10 +15,9 @@ df2 = pd.read_excel('data/kokusei_R2_v2.xlsx', sheet_name=1, index_col=None, ski
 st.write(df1.columns)
 
 df1 = df1[df1["地域都道府県名"] == "26_京都府"]
-#df1 = df1[df1[:,4] == "a"]
+#st.write(df1["地域都道府県・市区町村名"])
+df1 = df1[df1["地域市などの別（地域識別コード）"] == 0]
 st.write(df1)
-st.write(df1["地域都道府県・市区町村名"])
-st.write(df1["地域市などの別（地域識別コード）"])
 
 #df[(df['age'] < 25) & (df['point'] > 65)]
 #df1 = df1[df1.iloc[:,4] == 0]
