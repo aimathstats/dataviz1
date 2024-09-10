c42e85a6c6fa2d2fbf258b8b144541681a3ea2ea
aimathstats <aimathstats@gmail.com>
2024-09-10 22:30:25 +0900

    Update app_preliminary.py

--- app_preliminary.py
+++ app_preliminary.py
@@ -47,13 +47,14 @@ with col3:
             The chart above shows some numbers I picked for you.
         ''')
 
-col1, col2, col3 = st.columns(3)
-with col1:
-    st.latex(r'''\sum_{k=0}^{n-1} ar^k = a \left(\frac{1-r^{n}}{1-r}\right)''')
-with col2:
-    st.latex(r'''\sum_{k=0}^{n-1} ar^k''')
-with col3:
-    st.latex(r'''\sum_{k=0}^{n-1} ar^k''')
+if selected == "model":
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        st.latex(a)
+    with col2:
+        st.latex(b)
+    with col3:
+        st.latex(c)
 
 if selected == "minimax thm":
     col1, col2, col3 = st.columns(3)
