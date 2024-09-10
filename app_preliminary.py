3eff4308922046b0bf3c3483d8d06b6fbb3884b0
aimathstats <aimathstats@gmail.com>
2024-09-10 21:20:21 +0900

    Update app_preliminary.py

--- app_preliminary.py
+++ app_preliminary.py
@@ -46,6 +46,14 @@ with st.popover("Open popover"):
     name = st.text_input("What's your name?")
 st.write("Your name:", name)
 
+popover = st.popover("Filter items")
+red = popover.checkbox("Show red items.", True)
+blue = popover.checkbox("Show blue items.", True)
+if red:
+    st.write(":red[This is a red item.]")
+if blue:
+    st.write(":blue[This is a blue item.]")
+
 
 import pandas as pd
 import plotly.express as px
