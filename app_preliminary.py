1e45ca8314bb06f0474115191d5c7253456e2def
aimathstats <aimathstats@gmail.com>
2024-09-10 21:15:21 +0900

    Update app_preliminary.py

--- app_preliminary.py
+++ app_preliminary.py
@@ -35,22 +35,17 @@ container.write("This is inside too")
 
 
 st.subheader('empty')
-
 placeholder = st.empty()
-
 # Replace the placeholder with some text:
 placeholder.text("Hello")
-
 # Replace the text with a chart:
 placeholder.line_chart({"data": [1, 5, 2, 6]})
-
 # Replace the chart with several elements:
 with placeholder.container():
     st.write("This is one element")
     st.write("This is another")
-
 # Clear all those elements:
-placeholder.empty()
+#placeholder.empty()
 
 import pandas as pd
 import plotly.express as px
