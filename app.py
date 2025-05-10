513c3d9f8a34ad60c6641826f23fc31f6e25cb0d
aimathstats <aimathstats@gmail.com>
2025-05-10 12:11:09 +0900

    Update app.py

--- app.py
+++ app.py
@@ -16,6 +16,18 @@ pref_list = [
 ]
 st.map(pref_list)
 
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+# 東京駅を中心に擬似的な位置情報データを生成
+lat, lon = 35.68184, 139.76718
+df = pd.DataFrame(
+    np.random.randn(1000, 2) / [100, 100] + [lat, lon],
+    columns=['lat', 'lon']
+)
+st.map(df)
+
 # PDFからのテーブル取得と可視化：都道府県別コロナ定点観測の折れ線
 import fitz
 import requests
