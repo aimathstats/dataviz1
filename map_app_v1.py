08cf405951c0891fe132314805a699638ce85009
aimathstats <aimathstats@gmail.com>
2025-05-10 12:55:01 +0900

    Update map_app_v1.py

--- map_app_v1.py
+++ map_app_v1.py
@@ -2,12 +2,13 @@ import streamlit as st
 
 st.set_page_config(layout="wide")
 
-pref_list = [
+list = [
   {"latitude":35.051095034877825, "longitude":135.76477636253375}, #デフォルト現在地（植物園）
   {"latitude":35.04289379, "longitude":135.75676882}, #紫明小学校
   {"latitude":35.05044293, "longitude":135.75418841}, #元町小学校
 ]
 
 st.title("避難所マップ　version 1")
-st.map(pref_list)
+st.map(list)
 st.write("デフォルト現在地：京都府立植物園北門")
+st.write(list)
