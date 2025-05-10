c65e1e764fecec18e54361623675ab4a4304e0cf
aimathstats <aimathstats@gmail.com>
2025-05-10 11:54:54 +0900

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,14 @@ from pytrends.request import TrendReq
 
 st.set_page_config(layout="wide")
 
+# 簡単地図作成と地点表示、緯度経度データ（10進数）
+pref_list = [
+  {"longitude":130.741667, "latitude":32.789828}, # 熊本県
+  {"longitude":131.423855, "latitude":31.911090}, # 宮崎県
+  {"longitude":130.557981, "latitude":31.560148}, # 鹿児島県
+]
+st.map(pref_list)
+
 # PDFからのテーブル取得と可視化：都道府県別コロナ定点観測の折れ線
 import fitz
 import requests
