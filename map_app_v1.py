263b500844795fc2b529ccd0c8a751b4b40df837
aimathstats <aimathstats@gmail.com>
2025-05-11 15:40:08 +0900

    Update map_app_v1.py

--- map_app_v1.py
+++ map_app_v1.py
@@ -48,10 +48,9 @@ API_KEY = st.secrets["GOOGLE_API_KEY"]
 # 出発地と到着地の座標
 origin_coords = "35.04540,135.75870"      # 北大路駅
 destination_coords = "35.02332,135.75953" # 京都御所
-center_lat, center_lon = 35.02332, 135.75953
-a = np.random.randn(1,2)/[10,10] + [center_lat, center_lon] 
-print(a)
-destination_coords = f"a[0],a[1]"
+#center_lat, center_lon = 35.02332, 135.75953
+#a = np.random.randn(1,2)/[10,10] + [center_lat, center_lon] 
+#destination_coords = f"a[0],a[1]"
 
 # Directions APIリクエスト作成
 url = "https://maps.googleapis.com/maps/api/directions/json"
