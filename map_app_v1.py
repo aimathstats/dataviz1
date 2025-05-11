69dcf134aedd160aef5c8b115d6758b958165fb8
aimathstats <aimathstats@gmail.com>
2025-05-11 15:25:16 +0900

    Update map_app_v1.py

--- map_app_v1.py
+++ map_app_v1.py
@@ -47,8 +47,9 @@ API_KEY = st.secrets["GOOGLE_API_KEY"]
 # 出発地と到着地の座標
 origin_coords = "35.04540,135.75870"      # 北大路駅
 destination_coords = "35.02332,135.75953" # 京都御所
-a=destination_coords[1]+np.random.randn(1,1)/100
-b=destination_coords[2]+np.random.randn(1,1)/100
+st.write(destination_coords)
+#a=destination_coords[1]+np.random.randn(1,1)/100
+#b=destination_coords[2]+np.random.randn(1,1)/100
 st.write(a)
 st.write(b)
 destination_coords = "35.02332,135.75953"
