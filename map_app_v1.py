a7ac1fd773e91d4aaa42bff3a472e580e289ac1a
aimathstats <aimathstats@gmail.com>
2025-05-12 11:53:51 +0900

    Update map_app_v1.py

--- map_app_v1.py
+++ map_app_v1.py
@@ -71,6 +71,10 @@ params = {
 res = requests.get(url, params=params)
 data = res.json()
 
+# 確認のため，アプリ上にデータ表示
+st.write("取得したデータ")
+st.json(data)
+
 # レスポンスの確認
 if data.get("status") != "OK":
     st.error(f"Directions APIの取得に失敗しました: {data.get('status')}")
