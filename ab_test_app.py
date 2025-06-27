a5d9cc902d38cb285dcdf5e47322ed6a864078a9
aimathstats <aimathstats@gmail.com>
2025-06-28 08:16:26 +0900

    Update ab_test_app.py

--- ab_test_app.py
+++ ab_test_app.py
@@ -15,7 +15,7 @@ if 'ad_type' not in st.session_state:
     st.session_state.start_time = time.time()
 
 st.title("Web広告のABテスト")
-st.subheader(f"あなたに表示された広告：**{st.session_state.ad_type}**")
+st.subheader(f"広告：**{st.session_state.ad_type}**")
 #st.write("このページに滞在した時間を記録します。「滞在完了」ボタンを押すと記録されます。")
 
 # 滞在時間の記録
@@ -46,7 +46,7 @@ if st.button("滞在完了（記録）して画面更新"):
 
 # データ処理と分析
 st.divider()
-st.write("※以下はユーザーから見えない）")
+st.write("※以下に示されるデータ・データ分析はユーザーから見えません")
 st.subheader("A/B広告の滞在時間データ")
 
 if os.path.exists(DATA_FILE):
