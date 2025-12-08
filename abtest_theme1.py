9613fce345b0f103599d808cc967b9132ee7e246
aimathstats <aimathstats@gmail.com>
2025-12-08 14:30:58 +0900

    Update title and comments for AB test theme 1

--- abtest_theme1.py
+++ abtest_theme1.py
@@ -20,9 +20,9 @@ if 'ad_type' not in st.session_state:
     st.session_state.ad_type = random.choice(['A', 'B'])
     st.session_state.start_time = time.time()
 
-st.title("Web広告のABテスト")
-st.write("広告AまたはBをそれぞれ確率1/2で表示する")
-st.subheader(f"あなたの広告：**{st.session_state.ad_type}**")
+st.title("テーマ1のABテスト")
+#st.write("画像AまたはBをそれぞれ確率1/2で表示する")
+#st.subheader(f"画像：**{st.session_state.ad_type}**")
 st.image(ad_images[st.session_state.ad_type], width=200)
 
 # 滞在時間の記録
